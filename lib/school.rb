5ce24597d7c204f4cfccb0747ac5a6b4bcbcd76c
ktochow1 <ktochow1@binghamton.edu>
2018-11-02 18:59:12 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
 # code here!
+
+class School
+  
+  attr_accessor :roster 
+  
+  def initialize(school)
+    @school = school
+    @roster = {}
+ end
+
+
+
+  def school
+    @school
+  end
+  
+  def add_student(grade, name)
+    @roster[name] ||= []
+    @roster[name] << grade
+  end
+  
+ 
+  def grade(grade)
+    @roster[grade].flatten
+  end
+
+  def sort
+    @roster.values.sort
+  end
+  
+#   sorted = change.sort # Ascending sort
+# sorted = change.sort { |a, b| a <=> b } # Same thing!
+# sorted
+
+end
+
